1f9d79145128fdaabba05cbfe5349df703bea537
StegaDP <stegailodp@gmail.com>
2026-02-05 00:26:26 -0500

    Initialize Shoppex iOS project

---
+++ Shoppex/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shoppex
+//
+//  Created by Dmitrii Stegailo on 2026-02-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
